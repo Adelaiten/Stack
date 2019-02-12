fb24a8f9382f1fbf07756c5ac72eb7445b5747d7
Adelaiten <karoltrzaska19@gmail.com>
2019-02-12 13:46:49 +0100

    stack

---
+++ src/main/java/Stack.java
@@ -0,0 +1,52 @@
+
+public class Stack<T> {
+    private T[] stack;
+    private int maximumCapacity;
+    private int index;
+
+
+    private Stack() {
+
+    }
+
+    public Stack(int size) {
+        this.stack = (T[]) new Object[size];
+        this.maximumCapacity = size;
+        this.index = 0;
+    }
+
+
+    public void push(T item) throws StackOverflowException{
+        if(index == maximumCapacity) {
+            throw new StackOverflowException("Stack overflow!");
+        }
+
+        this.stack[index++] = item;
+    }
+
+
+    public T pop() throws StackUnderflowException{
+        if(index == 0) {
+            throw new StackUnderflowException("Stack underflow!");
+        }
+        T poppedItem = this.stack[(index-1)];
+        this.stack[(index-1)] = null;
+        index--;
+        return poppedItem;
+    }
+
+
+    public T peek() {
+        return stack[(index-1)];
+    }
+
+
+    public int getMaximumCapacity() {
+        return this.maximumCapacity;
+    }
+
+
+    public int getSpaceLeft() {
+        return this.maximumCapacity - index;
+    }
+}
---
+++ src/main/java/StackUnderflowException.java
@@ -0,0 +1,5 @@
+public class StackUnderflowException extends Exception {
+    public StackUnderflowException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}
